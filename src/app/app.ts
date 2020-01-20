ff5a260243fe0c88025c51dcd8b302e959b7147a
dannyfile <filatov.dv@spbgut.ru>
2020-01-20 19:50:52 +0100

    first stable testing build w/ 6 tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,17 @@ import version1 from "../version1/App.vue";
 import version2 from "../version2/App.vue";
 import version3 from "../version3/App.vue";
 import version4 from "../version4/App.vue";
+import version5 from "../version5/App.vue";
+import version6 from "../version6/App.vue";
 
 export default createComponent({
     components: {
         version1,
         version2,
         version3,
-        version4
+        version4,
+        version5,
+        version6
     },
     setup(props, ctx) {
         const s = reactive({
